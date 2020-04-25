88ae69419769efcce0272a6b919dad2fb940d49d
Patrick Belanger <1848500+patrickbelanger@users.noreply.github.com>
2020-04-25 12:58:54 -0400

    * Initial commit (setting up project with Spring Boot, Thymeleaf)

---
+++ src/main/java/org/patrickbelanger/Application.java
@@ -0,0 +1,18 @@
+package org.patrickbelanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Patrick Belanger
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+	SpringApplication.run(Application.class, args);
+    }
+}
